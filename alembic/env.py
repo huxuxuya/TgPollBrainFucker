cd899a6187cb81532f320658898021b0292c8b7d
Fedor Timokhov <bzzzzzzzzzz@mail.ru>
2025-06-19 11:43:57 +0300

    Refactored alembic.ini to remove unnecessary comments and streamline configuration. Updated env.py to allow connection retrieval from context arguments, enhancing flexibility in migration execution. Updated binary cache files for env.py and test_database.py.

--- alembic/env.py
+++ alembic/env.py
@@ -84,11 +84,14 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = context.get_x_argument(as_dictionary=True).get('connection')
+
+    if connectable is None:
+        connectable = engine_from_config(
+            config.get_section(config.config_ini_section, {}),
+            prefix="sqlalchemy.",
+            poolclass=pool.NullPool,
+        )
 
     with connectable.connect() as connection:
         context.configure(
